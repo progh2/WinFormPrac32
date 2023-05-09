2dbb044311fc3dbc73409e3e30863c4733cb4039
Gihun Ham <ham@e-mirim.hs.kr>
2023-05-09 12:21:41 +0900

    급훈 프로그램(급조)

--- WinFormPrac32/Form1.cs
+++ WinFormPrac32/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormPrac32
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
